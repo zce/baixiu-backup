d61b5e98987d7412204c27482de861967a8b7692
zce <w@zce.me>
2017-07-01 22:14:42 +0800

    step-03 add config file

--- admin/index.php
+++ admin/index.php
@@ -5,5 +5,9 @@
  * @since   0.1.0 初始化
  * @version 0.1.0 初始化
  */
+
+// 载入配置文件
+$config = require('../config.php');
 ?>
 <h1>后台脚本</h1>
+<p><?php echo $config['BAIXIU_DB_HOST']; ?></p>
---
+++ config.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * 项目配置文件
+ *
+ * @since   0.1.0 初始化
+ * @version 0.1.0 初始化
+ */
+
+return array(
+  /**
+   * 数据库主机
+   */
+  'BAIXIU_DB_HOST' => '127.0.0.1',
+  /**
+   * 数据库名
+   */
+  'BAIXIU_DB_NAME' => 'baixiu',
+  /**
+   * 数据库用户名
+   */
+  'BAIXIU_DB_USER' => 'root',
+  /**
+   * 数据库密码
+   */
+  'BAIXIU_DB_PASSWORD' => 'wanglei',
+);
