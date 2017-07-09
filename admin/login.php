54789b90dd196f8350d277d67c14fe505054d687
zce <w@zce.me>
2017-07-09 14:40:22 +0800

    step-02-01

--- admin/login.php
+++ admin/login.php
@@ -1,3 +1,59 @@
+<?php
+/**
+ * 登录页
+ */
+
+require '../config.php';
+
+// 判断是否为表单提交请求
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+  // 是表单提交，接收表单数据，执行登录校验逻辑
+  if (!isset($_POST['email']) || $_POST['email'] == '' || !isset($_POST['password']) || $_POST['password'] == '') {
+    // 没有正确填写表单，错误提示
+    $message = '请正确填写表单！';
+  } else {
+    // 1. 接收表单提交内容
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+
+    // 2. 连接数据库查询用户是否存在
+
+    // 建立与数据库的连接
+    $connection = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+
+    // 如果数据库连接失败，打印错误信息
+    if (!$connection) {
+      die('Connect Error (' . mysqli_connect_errno() . ') ' . mysqli_connect_error());
+    }
+
+    // 执行 SQL 语句查询用户表中 email 与用户提交的 email 相等的用户
+    $result = mysqli_query($connection, sprintf("select * from users where email = '%s' limit 1;", $email));
+
+    // 获取查询结果中的数据
+    if ($result != false && mysqli_num_rows($result) == 1) {
+      // 存在该用户
+      $user = mysqli_fetch_assoc($result);
+
+      // 判断密码是否相同
+      if ($user['password'] == $password) {
+        // 登录成功
+
+        // 跳转到后台首页
+        header('Location: /admin/index.php');
+
+        // 结束执行
+        exit;
+      } else {
+        // 密码错误
+        $message = '用户名与密码不匹配！';
+      }
+    } else {
+      // 用户名不存在
+      $message = '用户名与密码不匹配！';
+    }
+  }
+}
+?>
 <!DOCTYPE html>
 <html lang="zh-CN">
 <head>
@@ -8,21 +64,26 @@
 </head>
 <body>
   <div class="login">
-    <form class="login-wrap">
+    <form class="login-wrap" action="login.php" method="post">
       <img class="avatar" src="/static/uploads/monkey.png">
-      <!-- 有错误信息时展示 -->
-      <!-- <div class="alert alert-danger">
-        <strong>错误！</strong> 用户名或密码错误！
-      </div> -->
+      <?php if (isset($message)) : ?>
+      <div class="alert alert-danger">
+        <strong>错误！</strong><?php echo $message; ?>
+      </div>
+      <?php endif; ?>
       <div class="form-group">
         <label for="email" class="sr-only">邮箱</label>
-        <input id="email" type="email" class="form-control" placeholder="邮箱" autofocus>
+        <?php if (isset($email)) : ?>
+        <input id="email" name="email" type="email" class="form-control" value="<?php echo $email; ?>" placeholder="邮箱" autofocus>
+        <?php else : ?>
+        <input id="email" name="email" type="email" class="form-control" placeholder="邮箱" autofocus>
+        <?php endif; ?>
       </div>
       <div class="form-group">
         <label for="password" class="sr-only">密码</label>
-        <input id="password" type="password" class="form-control" placeholder="密码">
+        <input id="password" name="password" type="password" class="form-control" placeholder="密码">
       </div>
-      <a class="btn btn-primary btn-block" href="index.php">登 录</a>
+      <button type="submit" class="btn btn-primary btn-block">登 录</button>
     </form>
   </div>
 </body>
---
+++ functions.php
@@ -0,0 +1,78 @@
+<?php
+/**
+ * 常用函数封装
+ */
+
+// 载入配置文件
+require 'config.php'
+
+/**
+ * 建立数据库连接
+ * @return MySQLi 数据库连接对象
+ */
+function connect () {
+  // 建立与数据库的连接
+  $connection = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+
+  // 如果数据库连接失败，打印错误信息
+  if (!$connection) {
+    die('Connect Error (' . mysqli_connect_errno() . ') ' . mysqli_connect_error());
+  }
+
+  return $connection;
+}
+
+/**
+ * 执行一个查询语句，返回查询到的数据
+ * @param  String $sql SQL 语句
+ * @return Array       查询到的数据
+ */
+function query ($sql) {
+  // 获取与数据库之间的连接
+  $connection = connect();
+
+  // 执行 SQL 语句，获取一个查询对象
+  $result = mysqli_query($connection, $sql);
+
+  // 创建一个空的数组，用于存放每一行的数据
+  $data = array();
+
+  // 查询失败返回空数据
+  if (!$result) return $data
+
+  // 遍历每一行，将每一行转换为一个关联数组或索引数组，放到数组中
+  while ($row = mysqli_fetch_array($result)) {
+    $data[] = $row;
+  }
+
+  // 释放查询资源
+  mysqli_free_result($result);
+
+  // 关闭数据库连接
+  mysqli_close($connection);
+
+  // 返回数据
+  return $data;
+}
+
+/**
+ * 执行一个非查询语句，返回执行语句后受影响的行数
+ * @param  String  $sql 非查询语句
+ * @return Integer      执行语句后受影响的行数
+ */
+function execute ($sql) {
+  // 获取与数据库之间的连接
+  $connection = connect();
+
+  // 执行 SQL 语句，获取一个查询对象
+  $result = mysqli_query($connection, $sql);
+
+  // 获取执行语句后受影响的行数
+  $affected_rows = mysqli_affected_rows($connection);
+
+  // 关闭数据库连接
+  mysqli_close($connection);
+
+  // 返回受影响行数
+  return $affected_rows;
+}
